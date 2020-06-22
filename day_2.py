a48650c638020f5a2b62f77fb165b2803c35cdfe
Douglasdai <1772458202@qq.com>
2020-06-22 23:12:08 +0800

    第一天晚上的内容
    python 数据类型

---
+++ day_2.py
@@ -0,0 +1,19 @@
+#number
+#type()显示数据类型
+a,b,c,d= 20 ,2.2,True, 4+3j
+print(type(a),type(b),type(c),type(d))
+#isinstance 可以判断
+a=111
+isinstance(a,int)
+
+
+#数值计算
+a=3
+b=5
+c=a+b
+print(c)
+print(a)
+print(b)
+
+#python中不可变数据为：number string tuple
+#可变数据为 ：list dictionary set
